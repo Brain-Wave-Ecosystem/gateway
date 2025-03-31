24d081ad4b3b2dcc7d965c74c14be9ff43e3d6ec
Nofre <davidprogram417@gmail.com>
2025-03-31 22:27:40 +0200

    fixed bug in logging middleware with response nil address

--- internal/middlewares/logging/logging.go
+++ internal/middlewares/logging/logging.go
@@ -11,7 +11,6 @@ func NewLoggingMiddleware(logger *zap.Logger) runtime.Middleware {
 		return func(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
 			logger.Debug("request", zap.String("method", r.Method), zap.String("path", r.URL.Path), zap.String("query", r.URL.RawQuery))
 			handlerFunc(w, r, pathParams)
-			logger.Debug("response", zap.Int("status", r.Response.StatusCode), zap.String("content-type", r.Header.Get("Content-Type")), zap.String("content-length", w.Header().Get("Content-Length")))
 		}
 	}
 }
